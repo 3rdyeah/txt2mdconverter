8f5241b32463572d6f26840448e674085c8af4db
changsong <song.chang@gaea.com>
2020-05-30 16:36:12 +0800

    delete useless import

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import java.io.File;
 import java.io.FileInputStream;
